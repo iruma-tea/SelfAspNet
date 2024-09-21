2ea3264b3d18b02dda2ddc8889c1fce43aa081c7
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-09-21 15:22:02 +0900

    SQLightでマイグレーションを実行

--- SelfAspNet/Controllers/HelloController.cs
+++ SelfAspNet/Controllers/HelloController.cs
@@ -3,10 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SelfAspNet.Controllers;
 
-public class HelloController: Controller
+public class HelloController : Controller
 {
     public IActionResult Index()
     {
         return Content("こんにちは、世界！");
+    }
+
+    public IActionResult Show()
+    {
+        ViewBag.Message = "こんにちは、世界！";
+        return View();
     }
 }
