5403d928f809e44213cf4bd9468013698c52acd0
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-10-26 10:42:30 +0900

    リスト4.15まで学習完了

--- SelfAspNet/Migrations/20241026000420_Article.cs
+++ SelfAspNet/Migrations/20241026000420_Article.cs
@@ -27,6 +27,21 @@ namespace SelfAspNet.Migrations
                 {
                     table.PrimaryKey("PK_Articles", x => x.Id);
                 });
+
+            migrationBuilder.InsertData(
+                table: "Articles",
+                columns: new[] { "Id", "Category", "CreatedAt", "LastUpdatedAt", "Title", "Url" },
+                                values: new object[,]
+                {
+                    { 1, "JavaScript", new DateTime(2023, 12, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 12, 22, 0, 0, 0, 0, DateTimeKind.Unspecified), "ますます便利になるTypeScript！", "https://codezine.jp/article/corner/992" },
+                    { 2, "JavaScript", new DateTime(2023, 12, 23, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 12, 24, 0, 0, 0, 0, DateTimeKind.Unspecified), "Remixを通じてWebを学ぶ", "https://codezine.jp/article/corner/942" },
+                    { 3, "JavaScript", new DateTime(2023, 12, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 12, 26, 0, 0, 0, 0, DateTimeKind.Unspecified), "Web Componentsを基礎から学ぶ", "https://codezine.jp/article/corner/927" },
+                    { 4, "Rails", new DateTime(2023, 12, 27, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 12, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), "Railsの新機能を知ろう！", "https://codezine.jp/article/corner/991" },
+                    { 5, "Rails", new DateTime(2023, 12, 29, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 12, 30, 0, 0, 0, 0, DateTimeKind.Unspecified), "Railsによるクライアントサイド開発入門", "https://codezine.jp/article/corner/919" },
+                    { 6, "Rust", new DateTime(2023, 12, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "現場で役立つRust入門", "https://atmarkit.itmedia.co.jp/ait/series/36943/" },
+                    { 7, "Rust", new DateTime(2024, 1, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 1, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), "基本からしっかり学ぶRust入門", "https://atmarkit.itmedia.co.jp/ait/series/24844/" }
+                }
+            );
         }
 
         /// <inheritdoc />
--- SelfAspNet/Models/Book.cs
+++ SelfAspNet/Models/Book.cs
@@ -6,14 +6,18 @@ namespace SelfAspNet.Models;
 public class Book
 {
     public int Id { get; set; }
+    [DataType(DataType.ImageUrl)]
+    // [UIHint("Isbn")]
     [Display(Name = "ISBN")]
     public string Isbn { get; set; } = String.Empty;
     [Display(Name = "タイトル")]
     public string Title { get; set; } = String.Empty;
+    [DataType(DataType.Currency)]
     [Display(Name = "価格")]
     public int Price { get; set; }
     [Display(Name = "出版社")]
     public string Publisher { get; set; } = String.Empty;
+    [DataType(DataType.Date)]
     [Display(Name = "刊行日")]
     public DateTime Published { get; set; }
     [Display(Name = "配布サンプル")]
