47552a6bd2eceadba96b966504c38cf1fc412c02
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-11-04 15:03:21 +0900

    リスト5.85まで学習完了

--- SelfAspNet/Lib/InOptionsAttribute.cs
+++ SelfAspNet/Lib/InOptionsAttribute.cs
@@ -1,11 +1,12 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace SelfAspNet.Lib;
 
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
-public class InOptionsAttribute : ValidationAttribute
+public class InOptionsAttribute : ValidationAttribute, IClientModelValidator
 {
     // 候補リストを表すプライベート変数
     private string _options;
@@ -14,6 +15,14 @@ public class InOptionsAttribute : ValidationAttribute
     {
         _options = options;
         ErrorMessage = "{0}は「{1}」のいずれかの値で指定します。";
+    }
+
+    public void AddValidation(ClientModelValidationContext context)
+    {
+        var attrs = context.Attributes;
+        attrs.TryAdd("data-val", "true");
+        attrs.TryAdd("data-val-inoptions", FormatErrorMessage(context.ModelMetadata.GetDisplayName()));
+        attrs.TryAdd("data-val-inoptions-opts", _options);
     }
 
     // プロパティの表示名と候補値リストでエラーメッセージを整形
