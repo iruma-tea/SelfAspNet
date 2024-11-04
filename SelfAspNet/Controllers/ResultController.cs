41795094dbb0be31e4fc2692503f9315e119c400
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-11-04 18:26:17 +0900

    リスト6.11まで学習完了

--- SelfAspNet/Controllers/ResultController.cs
+++ SelfAspNet/Controllers/ResultController.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SelfAspNet.Models;
 
 namespace SelfAspNet.Controllers;
@@ -42,5 +44,64 @@ public class ResultController : Controller
             bs = bs.Where(b => b.Published <= DateTime.Now);
         }
         return PartialView("_AjaxResult", bs);
+    }
+
+    public IActionResult Move()
+    {
+        return Redirect("https://wings.msn.to/");
+    }
+
+    public IActionResult Local()
+    {
+        return LocalRedirect("https://wings.msn.to/");
+    }
+
+    public async Task<IActionResult> Status(int? id)
+    {
+        var bs = await _db.Books.FindAsync(id);
+        if (bs == null)
+        {
+            return StatusCode(404);
+        }
+        return View("../Books/Details", bs);
+    }
+
+    public IActionResult Nothing()
+    {
+        return Empty;
+    }
+
+    public IActionResult Plain()
+    {
+        return Content("こんにちは、世界！", "text/plain", Encoding.UTF8);
+    }
+
+    public async Task<IActionResult> Csv()
+    {
+        var bs = await _db.Books.ToListAsync();
+        var data = new StringBuilder();
+        bs.ForEach(b =>
+            data.Append(string.Format(
+                $"{b.Id},{b.Isbn},{b.Title},{b.Price},{b.Publisher},{b.Published}\r\n")
+            )
+        );
+        Response.Headers.Append(
+            "Content-Disposition", "attachment;filename=data.csv");
+        return Content(data.ToString(), "text/comma-separated-values", Encoding.GetEncoding("UTF-8"));
+    }
+
+    public IActionResult Image(int id)
+    {
+        // 物理パスで指定する場合
+        // var path = $"C:/data/images/img_{id}.png";
+        // return PhysicalFile(path, "image/png", "sample.png");
+
+        var path = $"/images/img_{id}.png";
+        return File(path, "image/png", "sample.png");
+    }
+
+    public IActionResult Risk(string path)
+    {
+        return PhysicalFile(path, "application/octet-stream");
     }
 }
