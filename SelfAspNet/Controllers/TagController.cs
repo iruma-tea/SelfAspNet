98fbfdf1271cf12a14398a9b0e94118419c3f44f
iruma-tea <shusuke.think.pad@gmail.com>
2025-01-03 00:47:50 +0900

    リスト4.7の学習完了

--- SelfAspNet/Controllers/TagController.cs
+++ SelfAspNet/Controllers/TagController.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Razor.TagHelpers;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using SelfAspNet.Models;
 
 namespace SelfAspNet.Controllers;
@@ -14,5 +15,17 @@ public class TagController : Controller
     {
         _db = db;
         _manager = manager;
+    }
+
+    public async Task<IActionResult> SelectGroup()
+    {
+        var articles = _db.Articles.Select(a => new
+        {
+            Url = a.Url,
+            Title = a.Title,
+            Category = a.Category
+        });
+        ViewBag.Opts = new SelectList(articles, "Url", "Title", null, "Category");
+        return View(await _db.Articles.FindAsync(1));
     }
 }
