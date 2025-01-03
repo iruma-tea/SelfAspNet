08692765bdccd373296aa743b11f9cda8211ab0f
iruma-tea <shusuke.think.pad@gmail.com>
2025-01-03 17:34:27 +0900

    リスト4-12まで学習完了

--- SelfAspNet/Models/Book.cs
+++ SelfAspNet/Models/Book.cs
@@ -6,10 +6,13 @@ namespace SelfAspNet.Models;
 public class Book
 {
     public int Id { get; set; }
+    // [UIHint("Isbn")]
+    [DataType(DataType.ImageUrl)]
     [Display(Name = "ISBN")]
     public string Isbn { get; set; } = String.Empty;
     [Display(Name = "タイトル")]
     public string Title { get; set; } = String.Empty;
+    [DataType(DataType.Currency)]
     [Display(Name = "価格")]
     public int Price { get; set; }
     [Display(Name = "出版社")]
